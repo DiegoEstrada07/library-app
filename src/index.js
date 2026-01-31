4a79d3abe860b56612e4576966c42697909cfd24
Andrea <anntmoreno@gmail.com>
2026-01-31 13:57:17 -0800

    feat(index.js): add routes for home and login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Usser from './usser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
- 
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Usser />} />
+    </Routes>
+  </BrowserRouter>
 );
-
